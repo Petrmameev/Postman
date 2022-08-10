edbc753807babb21aff78b2a3c188576ab0db0c1
Petr <petr_mameev@mail.ru>
2022-08-10 21:01:11 +0700

    succeess post commit

--- src/test/java/PostmanEchoTest.java
+++ src/test/java/PostmanEchoTest.java
@@ -15,7 +15,7 @@ class PostmanEchoTest {
 // Проверки
                 .then()
                 .statusCode(200)
-                .body("data", equalTo("Helo"))
+                .body("data", equalTo("Hello"))
         ;
     }
 }
