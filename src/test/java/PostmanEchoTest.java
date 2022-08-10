a4fb8fb296a63edc3fc9ff187599fe5e1e15dd80
Petr <petr_mameev@mail.ru>
2022-08-10 20:52:54 +0700

    fail post commit

--- src/test/java/PostmanEchoTest.java
+++ src/test/java/PostmanEchoTest.java
@@ -1,15 +1,21 @@
-public class PostmanEchoTest {
-    // Given - When - Then
-// Предусловия
-    given()
-  .baseUri("https://postman-echo.com")
-  .body("some data") // отправляемые данные (заголовки и query можно выставлять аналогично)
+import org.junit.jupiter.api.Test;
+import static io.restassured.RestAssured.given;
+import static org.hamcrest.Matchers.*;
+
+class PostmanEchoTest {
+    @Test
+    void shouldReturnRequest() {
+        given()
+                .baseUri("https://postman-echo.com")
+                .body("Hello") // отправляемые данные (заголовки и query можно выставлять аналогично)
 // Выполняемые действия
-.when()
-  .post("/post")
+
+                .when()
+                .post("/post")
 // Проверки
-.then()
-  .statusCode(200)
-  .body(/* --> ваша проверка здесь <-- */)
-            ;
+                .then()
+                .statusCode(200)
+                .body("data", equalTo("Helo"))
+        ;
+    }
 }
